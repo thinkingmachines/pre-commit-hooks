17e1ef8759b83a4ebf6ddcc8fb825ebba8a8b821
Mark Steve Samson <marksteve@thinkingmachin.es>
2019-03-22 14:08:35 +0800

    Fix prettier-standard hook install

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [
 
 if __name__ == "__main__":
     setup(
-        name="coding-style",
+        name="tm-coding-style",
         version="0.0.1",
         description="Thinking Machines Coding Style",
         install_requires=requirements,
